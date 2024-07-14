1794247f14766b294e235fab30cd878b64e32da4
aa2955 <aa2955@njit.edu>
2024-07-14 09:21:15 -0400

    Add files via upload

---
+++ bbtdash/src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Header from './components/header.js'
+import MainContent from './components/mainContent.js';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainContent />
+      
+    </div>
+  );
+}
+
+export default App;
