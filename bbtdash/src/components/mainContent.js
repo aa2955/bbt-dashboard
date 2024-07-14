dec2d14818b81678e73673c3e6e194ebd5a42ee6
aa2955 <aa2955@njit.edu>
2024-07-14 09:22:06 -0400

    Add files via upload

---
+++ bbtdash/src/components/mainContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <section id="home" className="main-content">
+      <div className="content-overlay">
+        <h2>Home Section</h2>
+        <p>This is the home section of the website.</p>
+      </div>
+    </section>
+  );
+}
+
+export default MainContent;
